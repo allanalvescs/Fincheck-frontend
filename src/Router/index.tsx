d85bf8a437ab813725b9dc508ee23eb68bbce3a4
Allan <allan.alves@controlle>
2023-12-30 16:10:49 -0300

    feat: create route /reegister and /login and create his pages

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,13 +1,24 @@
 import { Routes ,Route, BrowserRouter } from "react-router-dom"
+import { Authenticate } from "./Authenticate"
+import { Login } from "../View/Pages/Login"
+import { Register } from "../View/Pages/Register"
+import { Dashboard } from "../View/Pages/Dashboard"
+import { AuthLayout } from "../View/Layouts/AuthLayout"
 
 export function Router () {
     return (
     <BrowserRouter>
         <Routes>
-            <Route path="/login" element={<h1>Login</h1>}/>
-            <Route path="/register" element={<h1>Cadastro</h1>}/>
+            <Route element={<Authenticate />}>
+                <Route element={<AuthLayout />}>
+                    <Route path="/login" element={<Login />}/>
+                    <Route path="/register" element={<Register />}/>
+                </Route>
+            </Route>
 
-            <Route path="/" element={<h1>Dashboard</h1>}/>
+            <Route element={<Authenticate isPrivate/>}>
+                <Route path="/" element={<Dashboard />}/>
+            </Route>
         </Routes>
     </BrowserRouter>
     )
